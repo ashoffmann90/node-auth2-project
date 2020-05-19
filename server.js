0deadfe4ad3e863b7c2b476db119cff386722eab
Andrew Hoffmann <ashoffmann90@gmail.com>
2020-05-19 12:47:01 -0700

    server and file setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const userRouter = require('./users/user-router')
+const authRouter = require('./auth/auth-router')
+
+const server = express()
+
+server.use(express.json())
+server.use('/api/users', userRouter)
+server.use('/api/auth', authRouter)
+
+server.get("/", (req, res) => {
+    res.json({ api: "up" });
+  });
+
+  module.exports = server
